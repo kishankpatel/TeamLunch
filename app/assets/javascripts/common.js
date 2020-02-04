5014efcbfe1c3c3a87dcae518da52da62149c177
Kishan Patel <kishanptl.kp@gmail.com>
2020-02-04 16:46:46 +0530

    fixed datatable issue

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,8 +1,3 @@
-$(document).ready( function () {
-  $('#usersTable').DataTable();
-  $('#placesTable').DataTable();
-});
-
 $(document).on("click",".close-alert",function() {
   $(".alert").hide();
 });
